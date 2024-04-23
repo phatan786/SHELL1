79318f99e204f355d9416a2e417a345c70934f5e
vasim <aneeshaanu4578@gmail.com>
2024-04-23 23:36:04 +0530

    first commit

--- script.sh
+++ script.sh
@@ -3,8 +3,22 @@
 a=$1
 b=$2
 SCRIPTNAME=$0
+TOTALARG=$*
+TOTALARG=$@
+NUMBEROFARG=$#
+
+
 
 
 echo " my script anmes is $SCRIPTNAME"
 
 echo "a+b =$(($a+$b))"
+
+echo "total arg passed is $TOTALARG"
+
+echo "total arg passed is $TOTALARGG"
+
+echo "total number of args is $NUMBEROFARG"
+
+
+
