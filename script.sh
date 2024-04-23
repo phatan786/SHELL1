0eb06613b2503d993a086cf6992b9b1664ddbe6b
vasim <aneeshaanu4578@gmail.com>
2024-04-23 22:54:25 +0530

    first commit

--- script.sh
+++ script.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
+NAME=vasim
 
-echo "hello world"
-echo -e "\e[31mhello world\e[0m"
-echo -e "\e[32mgood\tmorning\e[om"
-echo -e "\e[1;33mthis\n\e[35mis\e[34m\nvasim\e[0m"
+
+echo "my name is $NAME"
