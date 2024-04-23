5132a43cef715a6510fe0786e3506f94371ef8d2
vasim <aneeshaanu4578@gmail.com>
2024-04-24 02:38:49 +0530

    jhbjhdsfa .

--- script.sh
+++ script.sh
@@ -4,20 +4,28 @@
 ID=$(id -u)
 
 
-if [ $ID -ne 0 ]; then
+VALIDATE(){
+if [ $1 -ne 0 ]; then
      echo "you are not root user u cannot continue the script"
      exit 1
      else
      echo "you are root user u can continue the script"
 fi
+}
 
-
-ls -l 
-
-if [ $? -ne 0 ]; then
-    echo "ls command is failure"
-    exit 1
-    else
-    echo "ls command is success"
+VALIDATE(){
+if [ $2 -ne 0 ]; then
+     echo "$3 is failure"
+     exit 1
+     else
+     echo "$3 is success"
 fi
+}
 
+
+
+#root user validation
+VALIDATE $ID
+
+ls -l
+VALIDATE $? lscommand
