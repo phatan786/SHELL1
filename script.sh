daf423be04fe8f340177468940b20391d2055f45
vasim <aneeshaanu4578@gmail.com>
2024-04-24 02:12:50 +0530

    jhbjhdsfa .

--- script.sh
+++ script.sh
@@ -10,3 +10,14 @@ if [ $ID -ne 0 ]; then
      else
      echo "you are root user u can continue the script"
 fi
+
+
+ls -l 
+
+if [$? -ne 0 ]; then
+    echo "ls command is failure"
+    exit 1
+    else
+    echo "ls command is success"
+fi
+
