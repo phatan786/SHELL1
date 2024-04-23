7099beb63784f85ae12567b8c9bd4e7a44da197b
vasim <aneeshaanu4578@gmail.com>
2024-04-23 22:53:17 +0530

    first commit

--- script.sh
+++ script.sh
@@ -3,4 +3,4 @@
 echo "hello world"
 echo -e "\e[31mhello world\e[0m"
 echo -e "\e[32mgood\tmorning\e[om"
-echo -e "\e[1;33mthis\nis\e[34m\nvasim\e[0m"
+echo -e "\e[1;33mthis\n\e[35mis\e[34m\nvasim\e[0m"
