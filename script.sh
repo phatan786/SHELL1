8c376311f1609de4b8d9a14c9104ddd82baadf1d
vasim <aneeshaanu4578@gmail.com>
2024-04-24 02:43:35 +0530

    jhbjhdsfa .

--- script.sh
+++ script.sh
@@ -4,22 +4,26 @@
 ID=$(id -u)
 
 
-VALIDATE(){
+VALIDATE () {
+
 if [ $1 -ne 0 ]; then
      echo "you are not root user u cannot continue the script"
      exit 1
      else
      echo "you are root user u can continue the script"
 fi
+
 }
 
-VALIDATE(){
+VALIDATE () {
+
 if [ $2 -ne 0 ]; then
      echo "$3 is failure"
      exit 1
      else
      echo "$3 is success"
 fi
+
 }
 
 
