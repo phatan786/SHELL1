eae9cbe98b20febf140962f1322f823e30e1c1fc
vasim <aneeshaanu4578@gmail.com>
2024-04-24 01:28:28 +0530

    first commit

--- script.sh
+++ script.sh
@@ -2,11 +2,12 @@
 
 read -p "please enter the name:"  name
 
-if [ $name == vasim ]; then
-       echo "script is sucess"
-       else
-       echo "script is failure"
+if [ $name != vasim ]; then
+       echo "eneterd name is not vasim u cannot continue to script"
        exit 1
+       else
+       echo "entered name is vasim u can continue the script"
+      
 fi
 
 echo "hello world"
