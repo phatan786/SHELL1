1ea680a605928cd99be0c1af694b1b04e83a289d
vasim <aneeshaanu4578@gmail.com>
2024-04-24 18:25:47 +0530

    jhdjhjkahkh

--- script.sh
+++ script.sh
@@ -3,11 +3,11 @@
 
 read -p "please enter value : " value
 
-if [ $value -ne 10 ]; then
+if [ $value -eq 10 ]; then
+   echo "entered value is equal to 10"
+   else
    echo "entered value is not equal to 10"
    exit 1
-   else
-   echo "entered value is equal to 10"
 fi
 
 
