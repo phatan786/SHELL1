8138b9a22e4abd95503c2a60edfb26dfe2b45b37
vasim <aneeshaanu4578@gmail.com>
2024-04-24 17:59:31 +0530

    jhdjhjkahkh

--- script.sh
+++ script.sh
@@ -1,22 +1,8 @@
 #!/bin/bash
 
-#before execution
+ID=$(id -u)
 
-
-name=$1
-fn=$2
-to=$@
-too=$*
-tooo=$#
-
-
-echo "my name is $name"
-
-echo "my friend name is $fn"
-
-echo  "total varaibles is $to $too"
-
-echo "total number of varaibles is $tooo"
+echo "login user id is $ID"
 
 
 
