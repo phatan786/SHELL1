5398f4bef036f650c0aa1fa1a725adf4a7aa4589
vasim <aneeshaanu4578@gmail.com>
2024-04-24 18:23:37 +0530

    jhdjhjkahkh

--- script.sh
+++ script.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 
-read -p "please enter name : " name
+read -p "please enter value : " value
 
-if [ $name == vasim ]; then
-   echo "script is success"
+if [ $value -ne 10 ]; then
+   echo "entered value is not equal to 10"
+   exit 1
+   else
+   echo "entered value is equal to 10"
 fi
 
 
