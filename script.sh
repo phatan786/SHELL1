2c2de2ef881894113d6c04cab3e7fc3cbf1163f2
vasim <aneeshaanu4578@gmail.com>
2024-04-24 17:46:19 +0530

    jhdjhjkahkh

--- script.sh
+++ script.sh
@@ -1,39 +1,9 @@
-
 #!/bin/bash
 
-ID=$(id -u)
+name=vasim
+
+echo "hi my name is $name"
 
 
-VALIDATE () {
-
-if [ $1 -ne 0 ]; then
-     echo "you are not root user u cannot continue the script"
-     exit 1
-     else
-     echo "you are root user u can continue the script"
-fi
-
-}
-
-
-
-
-SUCCESS () {
-if [ $1 -ne 0 ]; then
-   echo "$2 is failure"
-   exit 1
-   else
-   echo " $2 is success"
-fi
-}
-
-
-
-
-#root user validation
-VALIDATE $ID 
-
-ls -l 
-SUCCESS $? ls
 
 
