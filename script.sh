88f9de4465c1bb95ffeeee42e95368b3593f39c7
vasim <aneeshaanu4578@gmail.com>
2024-04-24 18:32:27 +0530

    jhdjhjkahkh

--- script.sh
+++ script.sh
@@ -5,9 +5,6 @@ read -p "please provide the input " input
 
 if [ -z $input ]; then
    echo "inpuyt has been provided u can continue the script"
-   else
-   echo "input has not been provided u cannot continue the script"
-   exit 1
 fi
 
 
